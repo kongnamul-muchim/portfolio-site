c20b7ba21bb8ba071c09da74cfd908bce393d202
GuGu <gugu@portfolio.dev>
2026-05-20 07:13:55 +0000

    feat: 지역 필터/정렬 + API 프록시 설정

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/jobs/:path*',
+        destination: 'http://45.59.101.155:8000/api/jobs/:path*',
+      },
+    ]
   }
 };
 
